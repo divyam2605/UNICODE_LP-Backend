4555ff67c44a5886c97143814b70947d64f77c7a
divyam2605 <104719264+divyam2605@users.noreply.github.com>
2022-11-14 15:42:09 +0530

    Delete ProductsInventory directory

--- ProductsInventory/productShelf/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from . models import products
-
-class productsSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = products
-        fields = '__all__'
